800b1a935da4053bb19f3c52f85c513998df886c
saltus-izumi <izumi@saltus.jp>
2025-12-26 19:20:59 +0900

    DBの文字コードなど調整

--- docker/postgres/initdb.d/hozen_ddl.sql
+++ docker/postgres/initdb.d/hozen_ddl.sql
@@ -1,3 +1,7 @@
 CREATE ROLE hozen LOGIN PASSWORD 'hozen#1qaz';
-CREATE DATABASE hozen encoding='UTF8';
+CREATE DATABASE hozen
+    WITH ENCODING 'UTF8'
+        LC_COLLATE 'ja_JP.UTF-8'
+        LC_CTYPE 'ja_JP.UTF-8'
+        TEMPLATE template0;
 GRANT ALL ON DATABASE hozen TO hozen;
